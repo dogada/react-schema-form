69f120026b6df46e091d74c984ed663e77645603
Jordan Frankfurt <jordanwfrankfurt@gmail.com>
2018-09-18 16:07:50 -0400

    makes the example work with new packages and removes unneeded ones

--- example/main.js
+++ example/main.js
@@ -1,13 +1,17 @@
-import React from 'react';import ReactDOM from 'react-dom';
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createMuiTheme, MuiThemeProvider} from '@material-ui/core/styles'
+import ExamplePage from './ExamplePage'
 
-const ExamplePage  = require('./ExamplePage');
 const theme = createMuiTheme({
     typography: {
         fontSize: 22,
-      },
-});
+    },
+})
 
-ReactDOM.render(<MuiThemeProvider theme={theme}>
-                    <ExamplePage />
-                </MuiThemeProvider>, document.getElementById("app"));
+ReactDOM.render(
+    <MuiThemeProvider theme={theme}>
+        <ExamplePage/>
+    </MuiThemeProvider>,
+    document.getElementById('app')
+)
